0773c007c7dfe9062a050d4d1c80333d30443fa1
Jailton Júnior <jailton.junior94@outlook.com>
2019-12-29 16:25:32 -0300

    conclusão do CartBloc

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:shopping/blocs/home.bloc.dart';
-import 'package:shopping/ui/android/pages/home.page.dart';
+import 'package:shopping/blocs/cart.bloc.dart';
+import 'package:shopping/ui/android/pages/tabs.page.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<HomeBloc>.value(value: HomeBloc()),
+        ChangeNotifierProvider<CartBloc>.value(value: CartBloc()),
       ],
       child: Main(),
     );
@@ -25,7 +27,10 @@ class Main extends StatelessWidget {
       title: 'Shopping Cart',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomePage(),
+      home: DefaultTabController(
+        length: 3,
+        child: TabsPage(),
+      ),
     );
   }
 }
