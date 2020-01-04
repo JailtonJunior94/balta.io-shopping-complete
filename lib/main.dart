3548f5ca7189e8a22aef605d0c229a878d410b2e
Jailton Júnior <jailton.junior94@outlook.com>
2020-01-03 21:14:07 -0300

    conclusão do curso (shopping)

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import 'package:shopping/blocs/home.bloc.dart';
 import 'package:shopping/blocs/cart.bloc.dart';
+import 'package:shopping/blocs/theme.bloc.dart';
 import 'package:shopping/blocs/user.bloc.dart';
 import 'package:shopping/ui/android/pages/tabs.page.dart';
 
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider<HomeBloc>.value(value: HomeBloc()),
         ChangeNotifierProvider<CartBloc>.value(value: CartBloc()),
         ChangeNotifierProvider<UserBloc>.value(value: UserBloc()),
+        ChangeNotifierProvider<ThemeBloc>.value(value: ThemeBloc()),
       ],
       child: Main(),
     );
@@ -25,10 +27,11 @@ class MyApp extends StatelessWidget {
 class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final ThemeBloc bloc = Provider.of<ThemeBloc>(context);
     return MaterialApp(
       title: 'Shopping Cart',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: bloc.theme,
       home: DefaultTabController(length: 3, child: TabsPage()),
     );
   }
